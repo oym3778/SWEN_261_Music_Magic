ca1a40638bade4e9b4b61502bde80958210c98a7
seang04 <seangaines04@gmail.com>
2023-10-16 18:17:30 -0400

    added test for Need.toString()

---
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/model/NeedTest.java
@@ -0,0 +1,31 @@
+package com.ufund.api.ufundapi.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Tag; 
+import org.junit.jupiter.api.Test;  
+
+
+@Tag("Model-tier")
+public class NeedTest {
+
+    /**
+     * @author Sean Gaines
+     */
+    @Test
+    public void testToString(){
+        int id = 99; 
+        String name = "Piano"; 
+        double price = 11.59; 
+        int quanity = 2; 
+
+        Need need = new Need(id, name, price, quanity); 
+
+        String expected = String.format(Need.STRING_FORMAT, id, name, price, quanity); 
+        String actual = need.toString(); 
+
+        assertEquals(expected, actual);
+
+
+    }
+
+}
