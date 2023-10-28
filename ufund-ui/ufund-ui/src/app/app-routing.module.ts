b0b6e5f2c03a90184f11214a1928896ecf50b86f
Omar Morales-Saez <oym3778@g.rit.edu>
2023-10-28 18:37:49 -0400

    ''

--- ufund-ui/ufund-ui/src/app/app-routing.module.ts
+++ ufund-ui/ufund-ui/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NeedDetailComponent } from './need-detail/need-detail.component'
+import { NeedsComponent } from './needs/needs.component'
+
+const routes: Routes = [
+  { path: 'detail/:id', component: NeedDetailComponent },
+  { path: 'needs', component: NeedsComponent },
+];
 
 @NgModule({
   imports: [
--- ufund-ui/ufund-ui/src/app/app.module.ts
+++ ufund-ui/ufund-ui/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NeedsComponent } from './needs/needs.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NeedDetailComponent } from './need-detail/need-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NeedsComponent
+    NeedsComponent,
+    NeedDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
