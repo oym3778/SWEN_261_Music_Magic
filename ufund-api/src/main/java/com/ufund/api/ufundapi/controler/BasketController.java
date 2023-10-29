4db6afa5dc87a60f8ca708a60a684dc5079af289
Teddy Davies <tod3496@rit.edu>
2023-10-29 15:58:34 -0400

    Basket Controller has fully functional getNeed, getNeeds, createNeed, and deleteNeed methods

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controler/BasketController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controler/BasketController.java
@@ -6,13 +6,18 @@ import java.util.logging.Logger;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ufund.api.ufundapi.model.Need;
 import com.ufund.api.ufundapi.persistence.BasketDAO;
+import com.ufund.api.ufundapi.persistence.NeedDAO;
 
 /**
  * Handles the REST API requests for the Funding Basket
@@ -24,16 +29,18 @@ import com.ufund.api.ufundapi.persistence.BasketDAO;
 public class BasketController {
     private static final Logger LOG = Logger.getLogger(BasketController.class.getName());
     private BasketDAO basketDao;
+    private NeedDAO needDao;
 
     /**
-     * Creates a REST API controller to repond to requests
-     * 
-     * @param needDao The {@link NeedDAO Need Data Access Object} to perform CRUD operations
-     * <br>
-     * This dependency is injected by the Spring Framework
+     * /**
+     * Creates a REST API controller to repond to basket requests
+     *
+     * @param basketDAO The {@link BasketDAO Basket Data Access Object} to perform CRUD operations
+     * @param needDAO The {@link NeedDAO Need Data Access Object} to check if need exists in cupboard
      */
-    public BasketController(BasketDAO basketDAO) {
+    public BasketController(BasketDAO basketDAO, NeedDAO needDAO) {
         this.basketDao = basketDAO;
+        this.needDao = needDAO;
     }
 
     /**
@@ -62,7 +69,7 @@ public class BasketController {
     }
 
     /**
-     * Respponds to the GEO requests for all {@linkplain Need needs}
+     * Respponds to the GET requests for all {@linkplain Need needs}
      * 
      * @return ResponseEntity with array of {@link Need need} objects (may be empty) and 
      * HTTP status of OK
@@ -77,6 +84,66 @@ public class BasketController {
             Need[] needs = basketDao.getNeeds();
             return new ResponseEntity<Need[]>(needs,HttpStatus.OK);
         } 
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Creates a {@linkplain Need need} with the provided need object
+     * Only adds need to funding basket if it is also in need cupboard
+     * 
+     * @param need - The {@link Need need} to create
+     * 
+     * @return ResponseEntity with created {@link Need need} object and HTTP status of CREATED
+     * ResponseEntity with HTTP status of CONFLICT if {@link Need need} object already exists
+     * ResponseEntity with HTTP status of NOT_FOUDND if {@link Need need} object is not also in need cupboard
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PostMapping("/add")
+    public ResponseEntity<Need> createNeed(@RequestBody Need need) {
+        LOG.info("POST /basket " + need);
+
+        try {
+            if (needDao.getNeed(need.getId()) == null)
+                return new ResponseEntity<Need>(HttpStatus.NOT_FOUND);
+
+            Need newNeed = basketDao.createNeed(need);
+
+            if (newNeed != null)
+                return new ResponseEntity<Need>(newNeed,HttpStatus.CREATED);
+            else
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+    * Deletes a {@linkplain Need need} with the given id
+    * 
+    * @param id The id of the {@link Need need} to delete
+    * 
+    * @return ResponseEntity HTTP status of OK if deleted
+    * ResponseEntity with HTTP status of NOT_FOUND if not found
+    * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+    */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Need> deleteNeed(@PathVariable int id) {
+        LOG.info("DELETE /needs/" + id);
+
+        try {
+            Need need = basketDao.getNeed(id);
+            boolean success = basketDao.deleteNeed(id);
+            
+            if (success) 
+                return new ResponseEntity<Need>(need,HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
         catch(IOException e) {
             LOG.log(Level.SEVERE,e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
