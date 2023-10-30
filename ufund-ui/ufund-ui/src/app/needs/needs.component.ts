adfe98f05ad2f0cbe68f814042a90860c92a5c05
Omar Morales-Saez <oym3778@g.rit.edu>
2023-10-29 23:19:08 -0400

    fixed the update view per need and added an Add feature for needs

--- ufund-ui/ufund-ui/src/app/needs/needs.component.ts
+++ ufund-ui/ufund-ui/src/app/needs/needs.component.ts
@@ -17,8 +17,24 @@ import { NeedService } from '../need.service';
 export class NeedsComponent {
   needs: Need[] = []; //Array of all the needs to display.
 
+  // tempNeed: Need = {
+  //   id: 1,
+  //   name: 'testNeed',
+  //   price: 2,
+  //   quantity: 2,
+  // };
+
   //Inject NeedService dependency.
-  constructor(private needService: NeedService) {}
+  constructor(private needService: NeedService) { }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(needs: Need[]): number {
+    return needs.length > 0 ? Math.max(...needs.map(need => need.id)) + 1 : 11;
+  }
 
   //Update the stored needs array based on the values access by needService.
   getNeeds(): void {
@@ -28,6 +44,29 @@ export class NeedsComponent {
   //Update needs array when the component is initialized. 
   ngOnInit(): void {
     this.getNeeds();
+  }
+
+  
+  add(nameH: string, price: string, quantity: string): void {
+    // need to do other checks
+    nameH = nameH.trim();
+    const tempNeed: Need = {
+      id: 1,
+      name: nameH,
+      price: Number(price),
+      quantity: Number(quantity),
+    };
+    if (!nameH) { return; }
+
+    this.needService.addNeed(tempNeed)
+      .subscribe(need => {
+        this.needs.push(tempNeed);
+      });
+  }
+
+  delete(need: Need): void {
+    this.needs = this.needs.filter(n => n !== need);
+    this.needService.deleteNeed(need.id).subscribe();
   }
 
 }
