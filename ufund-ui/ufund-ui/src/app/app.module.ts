91e29e3800ae2370b2e144dbcda2a7fbe63be29a
seang04 <seangaines04@gmail.com>
2023-11-01 10:57:39 -0400

    modified funding basket and needs html and css to display the add and remove buttons in a slightly nicer way.

--- ufund-ui/ufund-ui/src/app/app.module.ts
+++ ufund-ui/ufund-ui/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HelperViewComponent } from './helper-view/helper-view.component';
 import { FundingBasketComponent } from './funding-basket/funding-basket.component';
 import { AddNeedComponent } from './add-need/add-need.component';
 import { AdminViewComponent } from './admin-view/admin-view.component';
+import { NeedFilterPipe } from './need-filter.pipe';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { AdminViewComponent } from './admin-view/admin-view.component';
     HelperViewComponent,
     FundingBasketComponent,
     AddNeedComponent,
-    AdminViewComponent
+    AdminViewComponent,
+    NeedFilterPipe
   ],
   imports: [
     BrowserModule,
