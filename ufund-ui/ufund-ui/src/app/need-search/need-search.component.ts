871bf9d441dc97a0fc0cedc201b2b37284013e39
seang04 <seangaines04@gmail.com>
2023-11-01 10:05:29 -0400

    Made the search term case insensitive by converting all searches to a lower case string

--- ufund-ui/ufund-ui/src/app/need-search/need-search.component.ts
+++ ufund-ui/ufund-ui/src/app/need-search/need-search.component.ts
@@ -22,7 +22,7 @@ export class NeedSearchComponent implements OnInit {
 
   // Push a search term into the observable stream.
   search(term: string): void {
-    this.needService.updateNeedsFilter(term); 
+    this.needService.updateNeedsFilter(term.toLowerCase()); 
   }
 
   ngOnInit(): void {
