94bcb5bd8ae57d15dc4f0bfec62be904f22959fd
seang04 <seangaines04@gmail.com>
2023-11-19 22:30:40 -0500

    updated BasketController tests to reflect new functionality

--- ufund-api/src/test/java/com/ufund/api/ufundapi/controller/BasketControllerTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/controller/BasketControllerTest.java
@@ -1,5 +1,6 @@
 package com.ufund.api.ufundapi.controller;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -37,13 +38,49 @@ public class BasketControllerTest {
      * a mock Need DAO
      */
     @BeforeEach
-    public void setupNeedController() {
+    public void setupNeedController() throws IOException{
         mockBasketDAO = mock(BasketDAO.class);
         mockNeedDAO = mock(NeedDAO.class);
         basketController = new BasketController(mockBasketDAO, mockNeedDAO);
-
-
+        testCupboard[0] = new Need(32, "Triangle", 32, 2);
+        testCupboard[1] = new Need(3, "Piano", 1, 2345);
+        testCupboard[2] = new Need(55, "Help", 1, 223); 
+        testBasket[0] = 32;
+        testBasket[1] = 3;
+        when(mockBasketDAO.getNeeds()).thenReturn(testBasket);
+        for(Need need: testCupboard){
+            when(mockNeedDAO.getNeed(need.getId())).thenReturn(need);
+        }
     }
 
-    
+    @Test
+    public void testGetNeeds() throws IOException{
+        
+        ResponseEntity<Need[]> response = basketController.getNeeds();
+        assertEquals(response.getBody()[0], testCupboard[0]);
+        assertEquals(response.getBody()[1], testCupboard[1]);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void testAddNeed() throws IOException{
+       
+        Need testNeed = new Need(55, "Help", 1, 223);
+        when(mockBasketDAO.addNeed(55)).thenReturn(true);
+        ResponseEntity<Need> response = basketController.addNeed(testNeed);
+        assertEquals(response.getBody(), testNeed);
+    }
+
+    @Test 
+    public void testDeleteNeed() throws IOException{
+        when(mockBasketDAO.removeNeed(32)).thenReturn(true);
+        ResponseEntity response = basketController.deleteNeed(32); 
+        assertEquals(response.getBody(), testCupboard[0]);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        response = basketController.deleteNeed(22);
+        assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
+    }
+
+
+
 }
