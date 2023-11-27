e5e891a3f8b256a743d36834e7a3a6d9cf6db400
seang04 <seangaines04@gmail.com>
2023-11-27 17:58:04 -0500

    fixed issues with audio.

--- ufund-ui/ufund-ui/src/app/login/login.component.ts
+++ ufund-ui/ufund-ui/src/app/login/login.component.ts
@@ -46,14 +46,14 @@ export class LoginComponent {
     { url: '/assets/img/the-nojo.jpeg', 
     title: 'The NOJO: New Orleans Jazz Orchestra', 
     body: 'Our organization helps fund the instruments and equipment donations to the NOJO Youth Orchestra, providing opportunities for selected high school students to learn the history, music, and styles of big band playing in an 18-piece orchestra', 
-    audio: '/assets/img/placeholder.ogg'},
+    audio: '/assets/audio/placeholder.ogg'},
     { url: '/assets/img/ben-frank-hs.webp', 
     title: 'Benjamin Franklin High School Music Department', 
     body: 'Based in New Orleans, Benjamin Franklin High School includes beginner through advanced level Orchestra and Band, along with Music Theory I and AP Music Theory classes. They need your help to fund their programs.', 
-    audio: '/assets/img/placeholder.ogg'},
+    audio: '/assets/audio/placeholder.ogg'},
     { url: '/assets/img/gnoyo.jpeg', 
     title: 'Greater New Orleans Youth Orchestra', 
     body: 'The Greater New Orleans Youth Orchestras programs includes five orchestras, a Young Artist Academy, an Endangered Instruments Program, a Chamber Music Program and Outreach programs. Combined, these programs ensure that music education and orchestral music are accessible to area youth. GNOYO also holds summer festivals, performances, and retreats for talented young musicians across the Greater New Orleans region.', 
-    audio: '/assets/img/placeholder.ogg'},
+    audio: '/assets/audio/placeholder.ogg'},
   ];
 }
